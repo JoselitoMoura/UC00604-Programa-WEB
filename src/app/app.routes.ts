6d4ba0f7c4303be9de3dc4eebbbffa523c6f429c
JoselitoMoura <joselitodemourabarbosa>
2025-11-27 21:02:22 +0000

    Alterações no app.routes.ts e correção de erros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { Component } from '@angular/core';
+import { Authentication } from './pages/authentication/authentication';
 import { Routes } from '@angular/router';
+import { UC00604ProgramaWeb } from './pages/uc00604-programa-web/uc00604-programa-web';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  /** nossa 1ª rota */
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: "welcome", loadComponent: () => import('./pages/welcome/welcome').then((moura) => moura.Welcome)},
+  /* rota padrão sem layseout */
+  {path: 'pages/uc00604', component: UC00604ProgramaWeb},
+  {path: 'pages/autenticacao', loadComponent: () => import('./pages/authentication/authentication').then(ze => ze.Authentication)},
+
+  /** nossa ultima rota */
+  {path: '**', loadComponent: () => import('./pages/welcome/welcome').then(c => c.Welcome)},
+];
